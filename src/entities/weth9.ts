4e62fdad777341e5c16fe0dc6bd77d327a62d5eb
blazejkrzak <blazej@m8b.pl>
2024-01-30 16:36:30 +0100

    Feature: Add LUKSO support for chainID 42 at 4.0.8 tag

--- src/entities/weth9.ts
+++ src/entities/weth9.ts
@@ -8,7 +8,7 @@ export const WETH9: { [chainId: number]: Token } = {
   [3]: new Token(3, '0xc778417E063141139Fce010982780140Aa0cD5Ab', 18, 'WETH', 'Wrapped Ether'),
   [4]: new Token(4, '0xc778417E063141139Fce010982780140Aa0cD5Ab', 18, 'WETH', 'Wrapped Ether'),
   [5]: new Token(5, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH', 'Wrapped Ether'),
-  [42]: new Token(42, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH', 'Wrapped Ether'),
+  [42]: new Token(42, '0x736C71587bc44FA341F5CA3874A7b6D898425509', 18, 'WLYX', 'Wrapped LYX'),
 
   [10]: new Token(10, '0x4200000000000000000000000000000000000006', 18, 'WETH', 'Wrapped Ether'),
   [69]: new Token(69, '0x4200000000000000000000000000000000000006', 18, 'WETH', 'Wrapped Ether'),
